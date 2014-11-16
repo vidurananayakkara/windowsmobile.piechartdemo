0634a6c1882afa8b97a0325b75c9816d6ef6845e
vidurananayakkara <vidura_nanayakkara@hotmail.com>
2014-11-16 17:37:40 +0530

    Improved architecture

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPChartDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
--- WPChartDemo/Properties/AssemblyInfo.cs
+++ WPChartDemo/Properties/AssemblyInfo.cs
@@ -1,17 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 [assembly: AssemblyTitle("WPChartDemo")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPChartDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("11beb183-6b97-40b7-82e0-3dfd121480cc")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
